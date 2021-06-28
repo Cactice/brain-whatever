ee2ab961284831c9952b0cfd9786d4febd4d2cb9
cactice <14835424+Cactice@users.noreply.github.com>
2021-06-28 04:05:43 +0000

    Update usePose

--- hooks/usePose.ts
+++ hooks/usePose.ts
@@ -1,55 +1,50 @@
 import { Camera } from '@mediapipe/camera_utils';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
-import { Pose, POSE_CONNECTIONS, ResultsListener } from '@mediapipe/pose';
+import {
+  NormalizedLandmarkList,
+  Pose,
+  POSE_CONNECTIONS,
+  ResultsListener,
+} from '@mediapipe/pose';
+import { RefObject, useEffect, useState } from 'react';
 
 export const usePose = ({
-  videoElement,
-  canvasElement,
+  videoRef,
 }: {
-  videoElement: HTMLVideoElement;
-  canvasElement: HTMLCanvasElement;
+  videoRef: RefObject<HTMLVideoElement>;
 }) => {
-  const onResults: ResultsListener = (results) => {
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
+  const [landmarkList, setLandmarkList] =
+    useState<NormalizedLandmarkList | null>(null);
 
-    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-      color: '#00FF00',
-      lineWidth: 4,
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    const onResults: ResultsListener = (results) => {
+      if (results.poseLandmarks) {
+        setLandmarkList([...results.poseLandmarks]);
+      }
+    };
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+      },
     });
-    drawLandmarks(canvasCtx, results.poseLandmarks, {
-      color: '#FF0000',
-      lineWidth: 2,
+    pose.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
     });
-    canvasCtx.restore();
-  };
-
-  const pose = new Pose({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-    },
-  });
-  pose.setOptions({
-    modelComplexity: 1,
-    smoothLandmarks: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5,
-  });
-  pose.onResults(onResults);
-
-  const camera = new Camera(videoElement, {
-    onFrame: async () => {
-      await pose.send({ image: videoElement });
-    },
-    width: 1280,
-    height: 720,
-  });
-  camera.start();
+    pose.onResults(onResults);
+    if (videoElement) {
+      const camera = new Camera(videoElement, {
+        onFrame: async () => {
+          await pose.send({ image: videoElement });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+  }, [videoRef]);
+  return landmarkList;
 };
